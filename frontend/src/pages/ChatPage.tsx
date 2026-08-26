3bd78452a653a230dc069c9cb4211848bfc780a9
Anukul-Chandra <anukulchandra5585@gmail.com>
2026-08-26 20:10:53 +0600

    style(chat): darken futuristic chat background

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -124,7 +124,7 @@ export function ChatPage() {
   }
 
   return (
-    <div className="docmind-page relative flex min-h-0 flex-col h-full">
+    <div className="docmind-page relative flex min-h-0 flex-col h-full bg-[#080B0A]">
       <div className="flex min-h-0 flex-1 flex-col overflow-y-auto overscroll-contain">
         {messages.length === 0 ? (
           <EmptyState onExample={(question) => void handleSend(question)} />
